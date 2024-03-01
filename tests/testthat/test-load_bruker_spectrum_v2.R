80856d8af57bcdd2633518c844e2c6ef7deddf8c
Tobias Schmidt <tobias.schmidt331@gmail.com>
2024-03-01 10:43:32 +0000

    Added load_spectrum_v2 functions and tests

---
+++ tests/testthat/test-load_bruker_spectrum_v2.R
@@ -0,0 +1,27 @@
+# Keep this test in a seperate file, because it's very slow. This is useful
+# because testthat runs test files in parallel, i.e. by distributing slow tests
+# across different files we ensure they can be also distributed across different
+# cores.
+library(testthat)
+
+test_that("1. load_bruker_spectrum_v2", {
+    x <- with(
+        testdir = "load_bruker_spectrum_v2/1",
+        inputs = c(urine_1 = "bruker/urine/urine_1"),
+        expr = {
+            spectrum_data <- load_bruker_spectrum_v2("urine_1")
+        }
+    )
+    expect_identical(str2(spectrum_data), c(
+        "List of 8",
+        " $ x                : num [1:131072] 131 131 131 131 131 ...",
+        " $ y                : num [1:131072] 0.001265 0.001003 0.000105 -0.000937 -0.001062 ...",
+        " $ x_ppm            : num [1:131072] 14.8 14.8 14.8 14.8 14.8 ...",
+        " $ length           : int 131072",
+        " $ ppm_range        : num 20",
+        " $ ppm_highest_value: num 14.8",
+        " $ ppm_lowest_value : num -5.22",
+        " $ ppm_step         : num 0.000153"
+    ))
+    expect_equal(dir(x$testdir), c("urine_1"))
+})
