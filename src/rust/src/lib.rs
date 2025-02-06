62c0f0555d1f196cce4cfbe82ffbe090aa282dd7
Sombke Maximilian <sombkemaximilian@gmail.com>
2025-02-06 18:03:34 +0100

    Remove private functions from Rust bindings

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,13 +1,11 @@
 use extendr_api::prelude::*;
 
-mod deconvolute_functions;
 mod deconvoluter;
 mod deconvolution;
 mod spectrum;
 
 extendr_module! {
     mod metabodecon;
-    use deconvolute_functions;
     use deconvoluter;
     use deconvolution;
     use spectrum;
