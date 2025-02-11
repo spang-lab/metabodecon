0b9efa89ec2fcac2301880136ebd871cc77d8bee
Sombke Maximilian <sombkemaximilian@gmail.com>
2025-02-11 13:59:44 +0100

    Remove Bruker functions from the Rust side
    - These will be added with serialization and deserialization again later

--- src/rust/src/spectrum.rs
+++ src/rust/src/spectrum.rs
@@ -59,46 +59,6 @@ impl Spectrum {
         }
     }
 
-    pub(crate) fn from_bruker(
-        path: &str,
-        experiment: u32,
-        processing: u32,
-        signal_boundaries: Vec<f64>,
-    ) -> Self {
-        if signal_boundaries.len() != 2 {
-            throw_r_error("signal_boundaries must be a vector of length 2");
-        }
-        let signal_boundaries = (signal_boundaries[0], signal_boundaries[1]);
-
-        match spectrum::Bruker::read_spectrum(path, experiment, processing, signal_boundaries) {
-            Ok(inner) => Self { inner },
-            Err(e) => throw_r_error(format!("{}", e)),
-        }
-    }
-
-    pub(crate) fn from_bruker_set(
-        path: &str,
-        experiment: u32,
-        processing: u32,
-        signal_boundaries: Vec<f64>,
-    ) -> List {
-        if signal_boundaries.len() != 2 {
-            throw_r_error("signal_boundaries must be a vector of length 2");
-        }
-        let signal_boundaries = (signal_boundaries[0], signal_boundaries[1]);
-        let spectra =
-            match spectrum::Bruker::read_spectra(path, experiment, processing, signal_boundaries) {
-                Ok(spectra) => spectra,
-                Err(e) => throw_r_error(format!("{}", e)),
-            };
-
-        List::from_values(
-            spectra
-                .into_iter()
-                .map(|spectrum| Robj::from(Spectrum { inner: spectrum })),
-        )
-    }
-
     pub(crate) fn chemical_shifts(&self) -> Vec<f64> {
         self.inner.chemical_shifts().to_vec()
     }
