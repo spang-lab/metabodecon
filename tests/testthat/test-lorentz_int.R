173268920f6a413abecbd8ed83c597cafb8b456b
Tobias Schmidt <tobias2.schmidt@ukr.de>
2025-04-18 15:29:00 +0200

    Fix test-lorentz_int.R

--- tests/testthat/test-lorentz_int.R
+++ tests/testthat/test-lorentz_int.R
@@ -1,21 +1,21 @@
+# Inputs
+sim_dir <- metabodecon_file("sim_subset")
+decon <- MetaboDecon1D_silent_sim(filepath = sim_dir, filename = "sim_01")
+A <- decon$A
+x0 <- decon$x_0
+lambda <- decon$lambda
+old <- decon$integrals[1, ]
 
 test_that("lorentz_int can produce backwards compatible results", {
-    decon <- generate_lorentz_curves_sim(sim[1])
-    A <- decon$A
-    x0 <- decon$x_0
-    lambda <- decon$lambda
-    old <- decon$integrals[1, ]
     new <- lorentz_int(x0, A, lambda, limits = c(0, max(decon$x_values) + 0.001))
-    # + 0.001 because we have an error in the orignal function where the
-    # scaled datapoints are shifted by 0.001 during integral calculation
+    # We add + 0.001 because there is an error in the orignal function where the
+    # limits for calculating the integral are set one "scaled data point" (i.e.
+    # 1/1000) too high. (In fact, setting any limits at all is inferior compared
+    # to using A * pi for calculating the integrals).
     expect_equal(old, new)
 })
 
 test_that("using `A * pi` instead of `integrals` produces similar results", {
-    decon <- generate_lorentz_curves_sim(sim[1])
-    A <- decon$A
-    x0 <- decon$x_0
-    lambda <- decon$lambda
     old <- decon$integrals[1, ]
     new <- (abs(A) * pi)
     err <- abs(old - new)
