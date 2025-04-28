7e65cf4edc4df9424c260a29810c982a20b7ce26
Tobias Schmidt <tobias2.schmidt@ukr.de>
2025-04-28 13:35:05 +0200

    Add test-align.R

--- tests/testthat/test-align.R
+++ tests/testthat/test-align.R
@@ -1,23 +1,21 @@
 library(testthat)
 
+# Prepare inputs
+sap_01 <- sap[[1]]
+sap_01_shifted <- simulate_spectrum(
+    name = "sap_01_shifted",
+    cs = sap_01$meta$simpar$cs,
+    x0 = sap_01$meta$simpar$x0 + 0.3,
+    A  = sap_01$meta$simpar$A,
+    lambda = sap_01$meta$simpar$lambda,
+    noise = sap_01$meta$simpar$noise
+)
+spectra <- as_spectra(list(sap_01, sap_01_shifted))
+decons <- deconvolute(spectra, smopts=c(1,3), delta=3, sfr=c(3.2,-3.2), verbose = FALSE)
+
 test_that("align works", {
 
-    # Prepare inputs
-    sap_01 <- sap[[1]]
-    sap_01_shifted <- simulate_spectrum(
-        name = "sap_01_shifted",
-        cs = sap_01$meta$simpar$cs,
-        x0 = sap_01$meta$simpar$x0 + 0.3,
-        A  = sap_01$meta$simpar$A,
-        lambda = sap_01$meta$simpar$lambda,
-        noise = sap_01$meta$simpar$noise
-    )
-    spectra <- as_spectra(list(sap_01, sap_01_shifted))
-    plot_spectra(spectra)
-    decons <- deconvolute(spectra, smopts=c(1,3), delta=3, sfr=c(3.2,-3.2))
-
-    # Do the alignment
-    aligns <- align(decons)
+    aligns <- align(decons, verbose = FALSE)
 
     # Check structure of returned object. Strategy: add all fields to the
     # decons object that we expect [align()] to add. At the end the objects
@@ -54,4 +52,63 @@ test_that("align works", {
     supal <- aligns$sap_01_shifted$sit$supal
     lambda <- aligns$sap_01_shifted$lcpar$lambda
     expect_equal(supal, lorentz_sup(cs, x0_al, A, lambda))
+})
+
+test_that("align can install its dependencies", {
+
+    # TEST PURPOSE: If `metabodecon` or `speaq` is installed via
+    # `install.packages()`, the dependencies `impute` and `MassSpecWavelet`
+    # may not be installed. In this case, the missing dependencies should be
+    # automatically detected and installed by [align()] when `install_deps` is TRUE.
+    #
+    # TEST STRATEGY: Remove all site libraries and unload the `speaq` dependencies.
+    # Then, add a new (empty) site library and attempt to call `align()`. The
+    # function should detect the missing dependencies in the new site library and
+    # install them.
+    #
+    # IMPLEMENTATION DETAILS: Since `speaq` depends on `impute` and
+    # `MassSpecWavelet`, we cannot unload these dependencies while `speaq` is
+    # still loaded. Therefore, we need to unload `speaq` as well. However, after
+    # removing the site libraries, R will no longer be able to find `speaq`. To
+    # address this, we need to copy the `speaq` installation to the new site
+    # library before calling `align()`. This ensures the expected scenario where
+    # `speaq` is available but its dependencies are missing.
+
+    # Load packages required by testthat before removing site libs
+    requireNamespace("waldo", quietly = TRUE)   # required by `expect_false(<bool>)`
+    requireNamespace("diffobj", quietly = TRUE) # required by `expect_false(<try-error>)`
+    requireNamespace("glue", quietly = TRUE)    # required to print failure messages
+
+    # Remember installation path of speaq
+    speaq_path <- system.file(package = "speaq")
+
+    # Remove existing site libs and instead add new (empty) site lib
+    tmp_lib <- norm_path(tmpfile("tmp-library/"))
+    dir.create(tmp_lib, recursive = TRUE)
+    old_libs <- .libPaths()
+    n_libs <- length(old_libs)
+    defer(.libPaths(old_libs))
+    .libPaths(c(tmp_lib, old_libs[n_libs]))
+
+    # Unload speaq, impute and MassSpecWavelet
+    deps <- c("impute", "MassSpecWavelet")
+    unloadNamespace("speaq")
+    unloadNamespace(deps[1])
+    unloadNamespace(deps[2])
+
+    # Copy speaq to new site lib
+    file.copy(speaq_path, tmp_lib, recursive = TRUE)
+
+    deps_installed <- sapply(deps, requireNamespace, quietly = TRUE)
+    expect_false(any(deps_installed))
+
+    # Try installation from binary
+    obj <- evalwith(
+        output = "captured",
+        message = "captured",
+        expr = aligns <- align(decons, install_deps = TRUE, verbose = FALSE)
+    )
+
+    deps_installed <- sapply(deps, requireNamespace, quietly = TRUE)
+    expect_true(all(deps_installed))
 })
