d97fc58aca592419e4b1a2aaa6f749b984307b30
Tobias Schmidt <tobias2.schmidt@ukr.de>
2025-04-28 12:30:32 +0200

    Expect binary install of mdrb to fail on Linux

--- tests/testthat/test-install_mdrb.R
+++ tests/testthat/test-install_mdrb.R
@@ -2,7 +2,7 @@ library(testthat)
 
 test_that("install_mdrb works", {
 
-    # Remove all site libs from to make sure mdrb is not pre-installed.
+    # Remove all site libs to make sure mdrb is not pre-installed.
     # Load packages required for the test before doing this.
     requireNamespace("waldo", quietly = TRUE)   # required by `expect_false(<bool>)`
     requireNamespace("diffobj", quietly = TRUE) # required by `expect_false(<try-error>)`
@@ -13,7 +13,8 @@ test_that("install_mdrb works", {
     old_libs <- .libPaths()
     n_libs <- length(old_libs)
     defer(.libPaths(old_libs))
-    .libPaths(c(tmp_lib, old_libs[n_libs]))
+    .libPaths(c(tmp_lib, old_libs[n_libs]), include.site = FALSE)
+
     mdrb_available <- check_mdrb()
     expect_false(mdrb_available)
 
@@ -24,10 +25,16 @@ test_that("install_mdrb works", {
         expr = x <- try(install_mdrb(ask = FALSE))
     )
     mdrb_available <- check_mdrb()
-    if (getRversion() <  numeric_version("4.2")) {
+    if (getRversion() <  numeric_version("4.2") || Sys.info()[["sysname"]] == "Linux") {
         expect_true(inherits(x, "try-error"))
         expect_false(mdrb_available)
     } else {
+        if (inherits(x, "try-error")) {
+            message("\nCaptured messages:")
+            message(paste("|", obj$message, collapse = "\n"))
+            message("Captured output:")
+            message(paste("|", obj$output, collapse = "\n"))
+        }
         expect_true(is.null(x))
         expect_true(mdrb_available)
     }
