c4de723df0af73ce13535fdcf217050457f6a268
toscm <tobias.schmidt331@gmail.com>
2026-02-23 15:08:18 +0100

    Update plot snapshots to account for update in test lib

--- tests/testthat/test-plot_spectrum.R
+++ tests/testthat/test-plot_spectrum.R
@@ -2,6 +2,7 @@ testthat::skip_on_cran()
 testthat::skip_on_ci()
 
 test_result <- test_that("plot_spectrum works", {
+    withr::local_output_sink(nullfile())
     tmp <- vdiffr::expect_doppelganger(
         title = "plot_spectrum",
         fig = test_plot_spectrum,
